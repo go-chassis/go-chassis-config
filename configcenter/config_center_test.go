0cb5c5e4938b92f3494eadc285a3243c7b46bcd7
Shawn <xiaoliang.tian@gmail.com>
2019-10-18 19:05:43 +0800

    remove apollo (#46)

--- configcenter/config_center_test.go
+++ configcenter/config_center_test.go
@@ -3,22 +3,9 @@ package configcenter_test
 import (
 	"github.com/go-chassis/go-chassis-config"
 	"github.com/go-chassis/go-chassis-config/configcenter"
-	"github.com/go-chassis/paas-lager"
-	"github.com/go-mesh/openlogging"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
-
-func init() {
-	log.Init(log.Config{
-		LoggerLevel:   "DEBUG",
-		EnableRsyslog: false,
-		LogFormatText: false,
-		Writers:       []string{"stdout"},
-	})
-	l := log.NewLogger("test")
-	openlogging.SetLogger(l)
-}
 
 func TestNewConfigCenter(t *testing.T) {
 	c, err := configcenter.NewConfigCenter(config.Options{
--- go.mod
+++ go.mod
@@ -2,11 +2,7 @@ module github.com/go-chassis/go-chassis-config
 
 require (
 	github.com/go-chassis/foundation v0.0.0-20190621030543-c3b63f787f4c
-	github.com/go-chassis/go-chassis v1.7.2 // indirect
-	github.com/go-chassis/paas-lager v1.0.2-0.20190328010332-cf506050ddb2
 	github.com/go-mesh/openlogging v1.0.1
 	github.com/gorilla/websocket v1.4.0
-	github.com/onsi/ginkgo v1.10.2 // indirect
-	github.com/onsi/gomega v1.7.0 // indirect
 	github.com/stretchr/testify v1.3.0
 )
